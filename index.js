69720c79d54c9d6d150969b3c764cb530a03bd4e
“santhosh9133” <santhoshsan9133@gmail.com>
2024-12-15 06:30:04 +0530

    roor changes in indexjs

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.listen(PORT, () => {
     console.log(`server started and running at ${PORT}`)
 });
 
-app.use('/home', (req, res) => {
+app.use('/', (req, res) => {
     res.send("<h1> App Is Running <h1/>")
 })
